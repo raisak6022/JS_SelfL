a3b29bbeaf3cae1552eb60d69487a598ad49c568
Rais Ahmed Khokhar <raisak6022@gmail.com>
2023-10-19 11:41:39 +0530

    01_variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 144553
+let accountEmail = "raisfljsjf@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+
+// accountId = 2  // not allowed 
+
+accountEmail = "htc@c.com"
+accountPassword = "49485729345"
+accountCity = "Bengaluru"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
