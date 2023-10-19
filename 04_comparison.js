d1b976545c46d4c79f58198e9beedce239392617
Rais Ahmed Khokhar <raisak6022@gmail.com>
2023-10-19 16:36:13 +0530

    comparisons

---
+++ 04_comparison.js
@@ -0,0 +1,26 @@
+// basic comparison
+
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+// line no. 13 se 19 tak en types ke comparisons ko avoid hi karte hai 
+// js me yaha par inconsistency hai 
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0); 
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined >= 0);
+
+// === is known as strict check
+
+console.log("2" === 2);
