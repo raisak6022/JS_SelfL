8f0ede6ca9843fa307a61cade58eda49a2d5bddf
Rais Ahmed Khokhar <raisak6022@gmail.com>
2023-10-19 16:04:24 +0530

    operations_investigation

---
+++ 01_basics/03_conversionOperationB.js
@@ -0,0 +1,41 @@
+let value = 3 
+let negValue = -value
+// console.log(negValue);
+
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = " hi"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);  // opt:12
+// console.log(1 + "2");  // opt:12
+// console.log("1" + "2");  // opt:12
+// console.log("1" + 2 + 2); // string phele hai to string me convert ho jayega saara   opt:122
+// console.log(1 + 2 + "2"); // Agar bbad me string hai to phele waale do add ho jayenge fir string me add ho jayega   opt:32
+
+
+// console.log( (3 + 4) * 5 % 3); // here sir specifies that code must be in the good format 
+
+
+// console.log(+true);  // opt: 1
+// console.log(+"");  // opt: 0
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+let result = gameCounter++;
+console.log(result);  // opt:100
+console.log(gameCounter);  // opt:101  now here the gameCounter is used as reference so that's why the value is 101
+
+
