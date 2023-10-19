da2a059a9a412ef77708b8ef59994b78955177f8
Rais Ahmed Khokhar <raisak6022@gmail.com>
2023-10-19 12:05:28 +0530

    02_datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,28 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3 + 3)  // we are using nodejs, not browser so that's why alert is not working
+
+// console.log(3
+//     +
+//     3)  // code readability should be high. we do not write our code in this way
+
+    console.log("Lobster");
+
+
+
+    let name = "Rais"
+    let age = 20
+    let isLoggedIn = false
+
+    // number =>  2 to power 53
+    // bigint
+    // string => ""
+    // boolean => true/false
+    // null => standalone value
+    // undefined => abhi value assign nahi hui hai
+    // symbol => uniqueness ke liye use hota hai
+
+    // object 
+
+    console.log(typeof undefined);  // undefined
+    console.log(typeof null);  // object
