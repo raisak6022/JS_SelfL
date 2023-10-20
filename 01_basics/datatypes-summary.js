6dd8b0c644dbaa34962673427e1779c9ed23aa5e
Rais Ahmed Khokhar <raisak6022@gmail.com>
2023-10-20 17:06:33 +0530

    datatypes summaries

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,48 @@
+// primitive
+ 
+
+// 7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
+
+
+const score = 100
+const scoreValue = 100.3  // number
+
+const isLoggedIn = false  // boolean
+const outSideTemp = null  // null
+let userEmail;  // undefined
+
+const id  = Symbol('123') // Symbol
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);  
+
+const bigNumber = 3325541654616543518654n  // bigInt
+
+
+// Reference  (Non - primitive)
+
+// Array, Objects, Functions
+
+
+const heros = ["shaktiman", "naagraj", "doga"]   // Array
+
+
+// object
+let muobj = {
+    name: "Rais",
+    age: 22,
+}
+
+
+// function
+const myFunction = function() {
+    console.log("hello g");
+}
+
+
+// typeChecking : this is how we check the type of any data
+
+console.log(typeof myFunction);
+
+
+// https://262.ecma-international.org/5.1/#sec-11.4.3  // for more information visit this site
