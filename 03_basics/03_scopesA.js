3a25b418a15f9b66790beb8a26d1067fc4533602
Rais Ahmed Khokhar <raisak6022@gmail.com>
2023-10-31 10:35:10 +0530

    renamed

---
+++ 03_basics/03_scopesA.js
@@ -0,0 +1,19 @@
+// var c = 437
+
+{} 
+
+let a = 300
+if (true) {
+
+    let a = 79;
+    const b = 49;
+    // c = 30; 
+    console.log(`The inner value of a is ${a}`);  // here the value of a is original value
+}
+
+
+
+
+console.log(`The outer value of a is ${a}`);  // here the value of a is redeclared  from outer variable
+// console.log(b);
+// console.log(c);
