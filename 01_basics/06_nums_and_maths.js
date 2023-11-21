316ac698b3c9fc0884e3679498a69e27c3c60bf4
Rais Ahmed Khokhar <raisak6022@gmail.com>
2023-11-21 10:46:27 +0530

    updated

--- 01_basics/06_nums_and_maths.js
+++ 01_basics/06_nums_and_maths.js
@@ -9,12 +9,13 @@ const balance = new Number(100);
 //
 // console.log(typeof balance);
 // console.log(balance.toString().length);
-// console.log(balance.toFixed(1));  // decimal no. k baad me kitne zeo dikhane hai
+// console.log(balance.toFixed(1));  // decimal no. k baad me kitne zeo dikhane hai 
 
 const otherNumber = 129.8549;
 // console.log(otherNumber.toPrecision(3));  // yeh value ko precised form me batata hai
 
 const hundreds = 1000000;
+// console.log(hundreds.toLocaleString()); // seprates the numbers into , in us standard by default
 // console.log(hundreds.toLocaleString('en-IN')); // converts into units like ekaii dhaaiii sekda hazar in india standard
 
 // Number.MAX_SAFE_VALUE => yeh batata hai ki kitne bade no tak value jayegi (range)
@@ -24,10 +25,10 @@ const hundreds = 1000000;
 
 // console.log(Math);
 
-// console.log(Math.abs(-4));
-// console.log(Math.round(4.3));
-// console.log(Math.ceil(4.3));
-// console.log(Math.floor(4.9));
+// console.log(Math.abs(-4));  // yaha par negative values ko positive me change kar deta hai
+// console.log(Math.round(4.3));  // yeh humne roundOff values deta hai
+// console.log(Math.ceil(4.3));   // ceiling wali value top value hi leti hai after decimal chahe kitna chota decimal hi kyun na ho
+// console.log(Math.floor(4.9));  //  floor wali value hi leta hai yeh after decimal chahe kitna bhi bada no. kyun na ho
 // console.log(Math.sqrt(4));
 // console.log(Math.min(4,5,9,7,1,2,45,65,));
 // console.log(Math.max(4,5,9,7,1,2,45,65,));
@@ -36,7 +37,7 @@ console.log(Math.random());  // this method is used for grnerating random number
 console.log(Math.random() * 10);  // *10 is used to shifting the no. towards left
 console.log((Math.random() * 10) + 1);  // +1 is used to avoid 0
 console.log(Math.floor(Math.random() * 10) + 1); // floor is used for avoiding the decimals part
-
+console.log(Math.floor((Math.random() * 10) ));  // if i want 0-9 then i do this
 
 
 // if we want to set range from (for e.g. 10 to 20)
