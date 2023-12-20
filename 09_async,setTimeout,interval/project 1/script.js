d6512eab33cd384ea16624132a2318472aba90f2
Rais Ahmed Khokhar <raisak6022@gmail.com>
2023-12-20 19:35:59 +0530

    Project on timeout and interval

---
+++ 09_async,setTimeout,interval/project 1/script.js
@@ -0,0 +1,33 @@
+// generation of the random number
+
+const randomColor = () => {
+  const hex = "0123456789abcdef";
+  //   console.log(typeof hex);
+  let color = "#";
+  for (let i = 0; i < 6; i++) {
+    color += hex[Math.floor(Math.random() * 16)];
+  }
+  console.log(color);
+  return color;
+};
+
+// console.log(randomColor());
+
+let intervalId;
+const startChangingColor = function () {
+  const changeBgColor = () => {
+    document.body.style.backgroundColor = randomColor();
+  };
+  if (!intervalId) {
+    intervalId = setInterval(changeBgColor, 1000);
+  }
+};
+
+const stopChangingColor = function () {
+  clearInterval(intervalId);
+  intervalId = null;
+};
+
+document.querySelector("#start").addEventListener("click", startChangingColor);
+
+document.querySelector("#stop").addEventListener("click", stopChangingColor);
