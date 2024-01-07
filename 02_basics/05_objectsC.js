f5133c223934c5285ef184e6557536421a5d08d5
Rais Ahmed Khokhar <raisak6022@gmail.com>
2024-01-07 12:33:19 +0530

    object and array desctructuring

--- 02_basics/05_objectsC.js
+++ 02_basics/05_objectsC.js
@@ -40,3 +40,13 @@ console.log(naam);
 // }
 
 // navbar(company = "Rais")  
+
+
+// vvvvv imp
+// Basic array destructuring
+const numbers = [1, 2, 3, 4, 5];
+const [first, second, ...rest] = numbers;
+
+console.log(first); // Output: 1
+console.log(second); // Output: 2
+console.log(rest); // Output: [3, 4, 5]
